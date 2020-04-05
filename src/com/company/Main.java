719b5b683c03952294340d381fcf0cb3c8ce8834
Alireza Eiji <AlirezaEiji151379@gmail.com>
2020-04-06 00:25:31 +0430

    this is the first git practice!!!

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello there please Enter a String");
+        Scanner input=new Scanner(System.in);
+        String inputString=input.nextLine();
+        System.out.println("your String and Line was:"+inputString);
+    }
+}
